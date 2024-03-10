7b5dd8287ed86c47d1733a12179cb1c14f35e550
adilsaid <adilsaid64@gmail.com>
2024-03-10 22:08:34 +0000

    refactoring

--- main.py
+++ main.py
@@ -1,42 +1,41 @@
 import requests as r
 from bs4 import BeautifulSoup
 
-# https://www.google.com/finance/quote/TSLA:NASDAQ]
-
-def parse_request(url):
+def fetch_html(url):
+    '''Fetch HTML content.'''
     resp = r.get(url)
-    print(resp.status_code)
-    soup = BeautifulSoup(resp.content, 'html.parser')
-    return soup
+    return resp.content
 
+def parse_html_for_price_info(html_content):
+    '''Gets Price Information from the HTML content.'''
+    soup = BeautifulSoup(html_content, 'html.parser')
+    # we want to target the div that has the data we want. which is 'data-last-price'
+    # get the first div that has the data-last-price attribute. True means, this attribute is set to some value.
+    price_div = soup.find('div', attrs={'data-last-price':True}) 
+    return price_div
 
-def convert_currency(cur1, cur2):
-    url = f'https://www.google.com/finance/quote/{cur1.upper()}-{cur2.upper()}'
-    soup = parse_request(url)
-
-    price_div = soup.find('div', attrs={'data-last-price':True})
-    print(price_div)
-    # return {'cur1':cur1, 
-    #         'cur2':cur2,
-    #         'conversion':float(price_div['data-last-price'])}
-    
+def get_currency_conversion(from_currency, to_currency):
+    """Converts currency from one to another."""
+    url = f'https://www.google.com/finance/quote/{from_currency.upper()}-{to_currency.upper()}'
+    html_content = fetch_html(url)
+    price_div = parse_html_for_price_info(html_content)
+    if price_div is not None:
+        return float(price_div['data-last-price'])
+    else: return None
 
 def get_price_information(ticker, exchange):
+    """Gets price information for a given ticker and exchange."""
     url = f'https://www.google.com/finance/quote/{ticker}:{exchange}'
-    soup = parse_request(url)
-
-    # we want to target the div that has the data we want. which is 'data-last-price'
-    price_div = soup.find('div', attrs={'data-last-price':True}) # get the first div that has the data-last-price attribute. True means, this attribute is set to some value.
-    
-    return {'ticker':ticker, 
-            'exchange':exchange, 
-            'price' : float(price_div['data-last-price']), 
-            'timestamp' : int(price_div['data-last-normal-market-timestamp']),
-            'currency' : str(price_div['data-currency-code']),
-            'to_USD' : convert_currency(str(price_div['data-currency-code']), 'USD')
-            }
-
+    html_content = fetch_html(url)
+    price_div = parse_html_for_price_info(html_content)
+    return {
+        'ticker': ticker,
+        'exchange': exchange,
+        'price': float(price_div['data-last-price']),
+        'timestamp': int(price_div['data-last-normal-market-timestamp']),
+        'currency': price_div['data-currency-code'],
+        'to_USD': get_currency_conversion(price_div['data-currency-code'], 'USD')
+    }
 
 if __name__ == '__main__':
     print(get_price_information('TSLA', 'NASDAQ'))
-    print(convert_currency('USD', 'CAD'))
