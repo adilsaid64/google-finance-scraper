6d1ce1388c98044aac31bfeede7aedb5da8b4293
semantic-release <semantic-release>
2025-11-16 21:47:35 +0000

    1.0.0
    Automatically generated by python-semantic-release

--- src/portfoliotracker/__init__.py
+++ src/portfoliotracker/__init__.py
@@ -1,4 +1,4 @@
 from .main import Stock, Position, Portfolio, display_portfolio_summary
 
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 __all__ = ["Stock", "Position", "Portfolio", "display_portfolio_summary",]
